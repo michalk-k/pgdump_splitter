bca47784c605e4a62a0235a017370e724479168f
Michal Bartak <michal.bartak@betsys.com>
2024-03-05 17:16:10 +0100

    added latest tag

--- docker.sh
+++ docker.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-APP_VERSION="0.9.0"
+APP_VERSION="0.9.1"
 DOCKER_NAME="registry.betsys.com/postgres/pgdump_splitter"
 
-
-# docker build --output=bin --target=binaries --build-arg="APP_VERSION=v0.0.1" --build-arg="GO_VERSION=1.21" --platform=darwin/arm64,linux/amd64,linux/arm64 --tag=$DOCKER_NAME:$APP_VERSION .
-
-docker buildx build --push --no-cache --build-arg="APP_VERSION=$APP_VERSION" --build-arg="GO_VERSION=1.21" --platform "linux/amd64,darwin/arm64" --tag=$DOCKER_NAME:$APP_VERSION .
+docker buildx build --push --no-cache --build-arg="APP_VERSION=$APP_VERSION" --build-arg="GO_VERSION=1.21" --platform "linux/amd64,darwin/arm64" --tag=$DOCKER_NAME:$APP_VERSION --tag=$DOCKER_NAME:latest .
 
