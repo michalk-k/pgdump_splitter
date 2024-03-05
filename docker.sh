60cfea1857c2480950d17ba3277a5adbba1f081e
Michal Bartak <michal.kozusznik@betsys.com>
2024-03-05 09:29:12 +0000

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -5,7 +5,7 @@ DOCKER_NAME="registry.betsys.com/postgres/pgdump_splitter"
 
 # docker build --output=bin --target=binaries --build-arg="APP_VERSION=v0.0.1" --build-arg="GO_VERSION=1.21" --platform=darwin/arm64,linux/amd64,linux/arm64 --tag=$DOCKER_NAME:$APP_VERSION .
 
-docker build --build-arg="APP_VERSION=v0.0.1" --build-arg="GO_VERSION=1.21" --platform=linux/amd64,darwin/arm64 --tag=$DOCKER_NAME:$APP_VERSION .
+docker build --build-arg="APP_VERSION=v0.0.1" --build-arg="GO_VERSION=1.21" --platform "linux/amd64,darwin/arm64" --tag=$DOCKER_NAME:$APP_VERSION .
 
 docker push $DOCKER_NAME:$APP_VERSION
 
