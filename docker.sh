f2a25e041038770d58e19b396e9c7f625298db81
Michal Bartak <michal.bartak@betsys.com>
2024-03-11 10:33:30 +0100

    update docker after merge from origin

--- docker.sh
+++ docker.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-APP_VERSION="0.9.0"
 DOCKER_NAME="registry.betsys.com/postgres/pgdump_splitter"
 
-docker build -ldflags="-X 'main.version=$(cat VERSION)'" --build-arg="GO_VERSION=1.21" --platform=linux/amd64,linux/arm64,darwin/arm64 --tag=$DOCKER_NAME:$APP_VERSION .
-
-docker push $DOCKER_NAME:$APP_VERSION
-
+docker buildx build --push --no-cache  -ldflags="-X 'main.version=$(cat VERSION)'" --build-arg="GO_VERSION=1.21" --platform=linux/amd64,linux/arm64,darwin/arm64 --tag=$DOCKER_NAME:$APP_VERSION .
